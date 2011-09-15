6b033b485b0d985760fd243d8a3f92b8d5c1fe94
GrahamKnopp <five11598@hotmail.com>
2011-09-15 14:28:24 -0400

    added all category seed

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,5 +1,3 @@
 class Attribute < ActiveRecord::Base
   has_many :valuefields
-  has_and_belongs_to_many :components
-  has_and_belongs_to_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
-  has_and_belongs_to_many :attributes
   has_and_belongs_to_many :components
   has_and_belongs_to_many :valuefields
 end
