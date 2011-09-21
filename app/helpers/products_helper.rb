ec8b3052b0815fdc0da5881be01108cc4b4f9d52
GrahamKnopp <five11598@hotmail.com>
2011-09-21 10:11:29 -0400

    sync up

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -13,7 +13,34 @@ module ProductsHelper
         @sp+= "<td> #{button_to "Show", {:action => "show", :id => prod.id }, :method => :get}\n"
         @sp+= "<td> #{button_to "Edit", edit_product_path(prod), :method => :get}\n"
         @sp+= "<td> #{button_to "Delete", { :action => "destroy", :id => prod.id }, :confirm => "Are You sure?", :method => :delete}\n"
-        @sp+= "</tr>\n"
+      @sp+= "</tr>\n"
     end
   end
+
+  def build_products
+
+
+    for cat in Component.all
+      if(cat.component_parents.length == 0)
+        print_permuted_products(cat, "")
+      end
+    end
+  end
+
+  def print_permuted_products(component, path)
+
+    path += component.name + "|"
+
+    if(component.components.length != 0)
+      for child in component.components
+
+        print_permuted_products(child, path)
+      end
+    else
+      logger.info(path + "\n")
+    end
+
+
+  end
+
 end
