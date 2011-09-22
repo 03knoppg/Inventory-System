eb2baf3054905bddde501a1774fb1c0de96e71bb
03knoppg <graham.knopp@gmail.com>
2011-09-22 15:34:21 -0400

    some css started

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -11,9 +11,9 @@ module CategoriesHelper
       if(parent_id != 0)
         @s+= "<tr>\n"
           @s+= "<td> #{"-"*depth} #{cat.name }</td>\n"
-          @s+= "<td> #{button_to "Show", {:action => "show", :id => cat.id }, :method => :get}\n"
-          @s+= "<td> #{button_to "Edit", edit_category_path(cat), :method => :get}\n"
-          @s+= "<td> #{button_to "Delete", { :action => "destroy", :id => cat.id }, :confirm => confirmation_message(cat, @all_categories_hash), :method => :delete}\n"
+          @s+= "<td>#{button_to "Show", { :action => "show", :id => cat.id}, :method => :get}\n"
+          @s+= "#{button_to "Edit", edit_category_path(cat), :method => :get}\n"
+          @s+= "#{button_to "Delete", { :action => "destroy", :id => cat.id }, :confirm => confirmation_message(cat, @all_categories_hash), :method => :delete}</td>\n"
         @s+= "</tr>\n"
       end
       print_categories(cat.id, depth+1)
